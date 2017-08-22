1e9b159b26b131371852f7f79aa68ef1d8fa23f5
Jonathan Liu <jonathan.liu137@gmail.com>
2017-08-22 01:08:30 -0400

    Bootstrap currentUser

--- frontend/ask_app.jsx
+++ frontend/ask_app.jsx
@@ -8,8 +8,14 @@ document.addEventListener("DOMContentLoaded", () => {
   let store;
   if (window.currentUser) {
     const preloadedState = {
-      session: { currentUser: window.currentUser}
+      session: {
+        currentUser: window.currentUser,
+        errors: []
+      }
     };
+
+    store = configureStore(preloadedState);
+    delete window.currentUser;
   } else {
     store = configureStore();
   }
