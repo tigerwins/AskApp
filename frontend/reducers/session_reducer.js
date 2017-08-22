a967937a9adccdf9aeb37617188b2c4802c70aba
Jonathan Liu <jonathan.liu137@gmail.com>
2017-08-21 23:57:55 -0400

    Complete sessionReducer

--- frontend/reducers/session_reducer.js
+++ frontend/reducers/session_reducer.js
@@ -1,5 +1,5 @@
 import merge from 'lodash/merge';
-// import { RECEIVE_CURRENT_USER, RECEIVE_ERRORS } from '../actions/session_actions';
+import { RECEIVE_CURRENT_USER, RECEIVE_ERRORS } from '../actions/session_actions';
 
 const nullUser = Object.freeze({
   currentUser: null,
@@ -10,6 +10,13 @@ const sessionReducer = (state = nullUser, action) => {
   Object.freeze(state);
 
   switch(action.type) {
+    case RECEIVE_CURRENT_USER:
+      const currentUser = action.currentUser;
+      return merge({}, nullUser, { currentUser });
+    case RECEIVE_ERRORS:
+    debugger
+      const errors = action.errors;
+      return merge({}, nullUser, { errors });
     default:
       return state;
   }
