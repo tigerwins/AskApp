a663782b57605012f6b2808bbc652684e1139275
Jonathan Liu <jonathan.liu137@gmail.com>
2017-08-23 16:19:21 -0400

    Start header

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
-import HeaderContainer from './header_container';
+import HeaderContainer from './content/header_container';
 import FrontPageContainer from './session/front_page_container';
+import HomeContainer from './questions/home_container';
 
 const App = () => {
   return (
     <div className="ask-app">
-      <ProtectedRoute path="/" component={HeaderContainer} />
+      <div className="outer-header">
+        <ProtectedRoute path="/" component={HeaderContainer} />
+      </div>
       <Switch>
-        <ProtectedRoute path="/questions" />
+        <ProtectedRoute path="/questions" component={HomeContainer} />
         <ProtectedRoute path="/questions/:id" />
         <ProtectedRoute path="/answer" />
         <ProtectedRoute path="/topics/:id" />
--- frontend/components/questions/home.jsx
+++ frontend/components/questions/home.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import QuestionFeedContainer from './question_feed_container';
+import QuestionPrompt from './question_prompt';
+// import Feeds from '../topics/feeds';
 
 const Home = (props) => {
   return (
     <div className="home-container">
       This is the home container.
+      <QuestionPrompt />
       <QuestionFeedContainer />
     </div>
   );
