aff2aff619b7a31f6a6b1ec1db05c817c6df4d79
Jonathan Liu <jonathan.liu137@gmail.com>
2017-08-23 14:53:58 -0400

    Complete auth--ready for Heroku push

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.create!(fname: "John", lname: "Smith", email: "jsmith@me.com", password: "password")
