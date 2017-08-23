8d61f48aab4c2b541e4c097535d6881b843cc327
Jonathan Liu <jonathan.liu137@gmail.com>
2017-08-23 19:31:09 -0400

    Style header and fix email alignment

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import HeaderContainer from './content/header_container';
 import FrontPageContainer from './session/front_page_container';
 import HomeContainer from './questions/home_container';
+import NewQuestionsContainer from './questions/new_questions_container';
 
 const App = () => {
   return (
@@ -12,11 +13,11 @@ const App = () => {
         <ProtectedRoute path="/" component={HeaderContainer} />
       </div>
       <Switch>
-        <ProtectedRoute path="/questions" component={HomeContainer} />
         <ProtectedRoute path="/questions/:id" />
-        <ProtectedRoute path="/answer" />
+        <ProtectedRoute path="/answer" component={NewQuestionsContainer} />
         <ProtectedRoute path="/topics/:id" />
         <Route exact path="/" component={FrontPageContainer} />
+        <Redirect to="/" />
       </Switch>
     </div>
   );
