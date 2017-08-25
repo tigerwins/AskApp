b24344f12328ca4b983cedf6f62c3d13cdc0aa00
Jonathan Liu <jonathan.liu137@gmail.com>
2017-08-25 09:55:37 -0400

    Comment out textarea resize script

--- app/assets/javascripts/modal-textarea.js
+++ app/assets/javascripts/modal-textarea.js
@@ -1,12 +1,15 @@
-
-const txt = document.getElementById("modal-textarea");
-
-for (let i = 0; i < txt.length; i++) {
-  txt[i].setAttribute('style', 'height:' + (txt[i].scrollHeight) + 'px;overflow-y:hidden;');
-  txt[i].addEventListener("input", OnInput, false);
-}
-
-function OnInput() {
-  this.style.height = 'auto';
-  this.style.height = (this.scrollHeight) + 'px';
-}
+// 
+// const txt = document.getElementsByClassName("text-box");
+// // debugger
+// // if (txt) {
+// for (let i = 0; i < txt.length; i++) {
+//   for (let j = 0; j < txt[i].length; j++) {
+//     txt[j].setAttribute('style', 'height:' + (txt[j].scrollHeight) + 'px;overflow-y:hidden;');
+//     txt[j].addEventListener("input", OnInput, false);
+//   }
+// }
+//
+// function OnInput() {
+//   this.style.height = 'auto';
+//   this.style.height = (this.scrollHeight) + 'px';
+// }
