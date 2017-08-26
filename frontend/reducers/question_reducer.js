9bae12c6960bdd634de9da42cfbf457d9ee7fa44
Jonathan Liu <jonathan.liu137@gmail.com>
2017-08-26 18:29:17 -0400

    Remove unnecessary 'removeQuestion' action from reducer

--- frontend/reducers/question_reducer.js
+++ frontend/reducers/question_reducer.js
@@ -2,7 +2,6 @@ import merge from 'lodash/merge';
 import {
   RECEIVE_QUESTIONS,
   RECEIVE_QUESTION,
-  REMOVE_QUESTION
 } from '../actions/question_actions';
 
 const questionReducer = (state = {}, action) => {
@@ -21,10 +20,6 @@ const questionReducer = (state = {}, action) => {
         [action.payload.question.id]: action.payload.question
       };
       return merge({}, state, newQuestion);
-    case REMOVE_QUESTION:
-      nextState = merge({}, state);
-      delete nextState[action.payload.question.id];
-      return nextState;
     default:
       return state;
   }
