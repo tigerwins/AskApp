39c4d08f06a51d1227a0dd5214a8064ec91d7d43
Jonathan Liu <jonathan.liu137@gmail.com>
2017-08-27 12:43:43 -0400

    spacing

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,15 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     resources :users, only: [:index, :create] #, :show]
     resource :session, only: [:create, :destroy]
+
     resources :questions, only: [:index, :create, :show, :update] do
       resources :answers, only: [:index, :create]
     end
+
     resources :answers, only: [:show, :update, :destroy] do
       resources :comments, only: [:index, :create]
     end
+
     resources :comments, only: [:destroy]
     # resources :topics, only: [:index, :create, :show]
 
