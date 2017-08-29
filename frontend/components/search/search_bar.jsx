e26cee75b94041c57b0991ac82d326f235f6467e
Jonathan Liu <jonathan.liu137@gmail.com>
2017-08-29 16:53:22 -0400

    Commit before testing react-autosuggest

--- frontend/components/search/search_bar.jsx
+++ frontend/components/search/search_bar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { searchQuestions } from '../../util/question_api_util';
 
 class SearchBar extends React.Component {
   constructor(props) {
@@ -9,21 +10,59 @@ class SearchBar extends React.Component {
       searchResults: [],
     };
 
+    this.focused = false;
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSearchClick = this.handleSearchClick.bind(this);
+    this.handleFocus = this.handleFocus.bind(this);
+    this.handleBlur = this.handleBlur.bind(this);
   }
 
-  handleChange(field) {
-    (e) => this.setState({ query: e.target.value }, () => )
+  handleChange(e) {
+    const self = this;
+    this.setState({ query: e.target.value }, () => searchQuestions(this.state.query).then(
+      searchResults => {
+        if (Object.prototype.toString.call(searchResults) === '[object Array]') {
+          self.setState({ searchResults: searchResults });
+        } else {
+          self.setState({ searchResults: [] });
+        }
+      })
+    );
   }
 
-  onFocus(e) {
+  handleSearchClick(questionId) {
+    return e => {
+      this.setState({ searchResults: [] });
+      this.props.requestQuestion(questionId).
+        then(this.props.history.push(`/questions/${questionId}`));
+    };
+  }
+
+  handleFocus(e) {
 
   }
 
-  onBlur(e) {
+  handleBlur(e) {
+    setTimeout(() => this.setState({ searchResults: [] }), 100);
+  }
 
+  renderResults() {
+    let results;
+    if (this.state.searchResults > 0) {
+      results = this.state.searchResults.map ((result, idx) => {
+        return (
+          <div className="search-result" key={idx}>
+
+          </div>
+        );
+      });
+    }
   }
 
   render() {
 
   }
 }
+
+export default SearchBar;
---
+++ frontend/components/search/search_bar_container.js
@@ -0,0 +1,17 @@
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import SearchBar from './search_bar';
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+
+  };
+};
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+    
+  };
+};
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(SearchBar));
