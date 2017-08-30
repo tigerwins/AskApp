625a64c471ecec91421cd36a7fea75ab6bbe17bd
Jonathan Liu <jonathan.liu137@gmail.com>
2017-08-30 10:08:00 -0400

    Add topicsReducer to entitiesReducer

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -3,12 +3,14 @@ import userReducer from './user_reducer';
 import questionReducer from './question_reducer';
 import answerReducer from './answer_reducer';
 import commentReducer from './comment_reducer';
+import topicReducer from './topic_reducer';
 
 const entitiesReducer = combineReducers({
   users: userReducer,
   questions: questionReducer,
   answers: answerReducer,
   comments: commentReducer,
+  topics: topicReducer,
 });
 
 export default entitiesReducer;
