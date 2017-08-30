1daf9daa4d38cc8b46c41d77017366d04bc89839
Jonathan Liu <jonathan.liu137@gmail.com>
2017-08-30 10:14:56 -0400

    Create TopicAPIUtil

---
+++ frontend/util/topic_api_util.js
@@ -0,0 +1,14 @@
+export const createQuestionTopic = questionTopic => {
+  return $.ajax({
+    method: "POST",
+    url: "/api/question_topics/",
+    data: { question_topic: questionTopic }
+  });
+};
+
+export const destroyQuestionTopic = questionTopic => {
+  return $.ajax({
+    method: "DELETE",
+    url: `/api/question_topics/${questionTopic.id}`,
+  });
+};
