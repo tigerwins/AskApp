059302ac2293d1e82e204fef8a0b309a68158b20
Jonathan Liu <jonathan.liu137@gmail.com>
2017-09-01 22:23:00 -0400

    Lower pg_search trigram search threshold

--- app/models/question.rb
+++ app/models/question.rb
@@ -19,7 +19,7 @@ class Question < ApplicationRecord
     using: {
       tsearch: { any_word: true, prefix: true },
       dmetaphone: { any_word: true, sort_only: true },
-      trigram: { threshold: 0.75 }
+      trigram: { threshold: 0.1 }
     }
 
   belongs_to :asker,
