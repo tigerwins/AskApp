f3a2f362332c37aad7902ffebecbfb0c248a5688
Jonathan Liu <jonathan.liu137@gmail.com>
2017-09-16 23:59:40 -0400

    Cleanup comments

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'static_pages#root'
 
   namespace :api, defaults: { format: :json } do
-    resources :users, only: [:index, :create] #, :show]
+    resources :users, only: [:index, :create]
     resource :session, only: [:create, :destroy]
 
     resources :questions, only: [:index, :create, :show, :update] do
